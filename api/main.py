253196849d2f9ea2f37f24161e242b961565ae21
Aditya Chavan <130660675+Aditya-A-Chavan@users.noreply.github.com>
2023-07-06 01:18:37 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,30 +1,38 @@
 from flask import Flask, render_template, request
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
+
+
 def weather():
     if request.method == 'POST':
         location = request.form['location']
-        api_key = 'c70f6f3bf64ab7edc365a23b47a6f5d6' 
+        api_key = 'c70f6f3bf64ab7edc365a23b47a6f5d6'
         url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
         response = requests.get(url)
         data = response.json()
+        url_geocoder = f'http://api.openweathermap.org/geo/1.0/direct?q={location}&limit=2&appid={api_key}'
+        response1 = requests.get(url_geocoder)
+        data1 = response1.json()
         if response.status_code == 200:
             weather_data = {
                 'location': data['name'],
                 'temperature': round(data['main']['temp']),
                 'condition': data['weather'][0]['description'],
                 'FeelsLike': round(data['main']['feels_like']),
-                'Mintemp': round(data['main']['temp_min']),
-                'Maxtemp': round(data['main']['temp_max'])
+                'MaxTemp': round(data['main']['temp_max']),
+                'MinTemp': round(data['main']['temp_min'])
             }
-            return render_template('weather.html', weather=weather_data)
+            latitude = {
+                'lat': data1[0]['lat'],
+                'long': data1[0]['lon']
+            }
+            return render_template('weather.html', weather=weather_data, latitude=latitude)
         else:
             error_message = data['message']
             return render_template('weather.html', error=error_message)
     return render_template('weather.html')
-
 if __name__ == '__main__':
     app.run(debug=True)
