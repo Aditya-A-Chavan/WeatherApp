26992dfb5f9a94b6588546d7845eee2d0a73fd6d
Aditya Chavan <130660675+Aditya-A-Chavan@users.noreply.github.com>
2023-07-05 14:17:59 +0530

    Update main.py
    migrated to OWM

--- api/main.py
+++ api/main.py
@@ -1,32 +1,29 @@
 from flask import Flask, render_template, request
 import requests
-import json
 
-print(json.__version__)
-print(requests.__version__)
+app = Flask(__name__, template_folder='templates')
 
-app = Flask(__name__, template_folder="templates")
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/weather')
-def get_weather():
-    location = request.args.get('location')
-    api_key = '2ef114a28a8c41aabc7173812232806'
-    url = f'https://api.weatherapi.com/v1/current.json?key={api_key}&q={location}'
-
-    response = requests.get(url)
-    data = json.loads(response.text)
-
-    weather_data = {
-        'location': data['location']['name'],
-        'temperature': data['current']['temp_c'],
-        'condition': data['current']['condition']['text']
-    }
-
-    return weather_data
+@app.route('/', methods=['GET', 'POST'])
+def weather():
+    if request.method == 'POST':
+        location = request.form['location']
+        api_key = 'c70f6f3bf64ab7edc365a23b47a6f5d6' 
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric'
+        response = requests.get(url)
+        print(url)
+        data = response.json()
+        if response.status_code == 200:
+            weather_data = {
+                'location': data['name'],
+                'temperature': round(data['main']['temp']),
+                'condition': data['weather'][0]['description'],
+                'FeelsLike': round(data['main']['feels_like'])
+            }
+            return render_template('weather.html', weather=weather_data)
+        else:
+            error_message = data['message']
+            return render_template('weather.html', error=error_message)
+    return render_template('weather.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
